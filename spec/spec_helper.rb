ce8ec3cfeba91ada0c7343dcf11a9dd32f5ef97a
Leandro Silva <leandrodoze@gmail.com>
2009-03-08 02:28:29 -0300

    adicionei uma nota sobre o OC4J tambem na Spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,9 @@ module SparrowHelperMethods
   #
   # Apenas cria, mas nao faz o setup do cliente JMS.
   #
+  # WARNING: OC4J will be used as Java EE application server, but any other could
+  # be used with no problems.
+  #
   def create_jms_client
     jms_client = Sparrow::JMS::Connection::Client.new do |properties|
       properties.client_jar_file         = '/home/leandro/Desenvolvimento/java/servers/oc4j_extended_101330/j2ee/home/oc4jclient.jar'
