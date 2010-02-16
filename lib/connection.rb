4bcd6a785ffb118de474947bb9a50f9fe7ba2e15
Leandro Silva <leandrodoze@gmail.com>
2010-02-15 16:02:46 -0800

    reverting mega fail

--- lib/connection.rb
+++ lib/connection.rb
@@ -118,11 +118,11 @@ module Sparrow
       end
 
       def queue_connection_factory
-        @jndi_name_of_connection_factories[:queue_connection_factory]
+        @connection_factories[:queue_connection_factory]
       end
 
       def topic_connection_factory
-        @jndi_name_of_connection_factories[:topic_connection_factory]
+        @connection_factories[:topic_connection_factory]
       end
 
       def enable_queues(jndi_names = {})
