42e375f4993f114a15321f9dcde81abab0deb6f1
Leandro Silva <leandrodoze@gmail.com>
2010-02-19 00:24:10 -0800

    more refactoring

--- spec/listener_spec.rb
+++ spec/listener_spec.rb
@@ -1,7 +1,21 @@
 #
-# Cenario pos-configuracao do cliente JMS, quando deve ser possivel escutar mensagens
-# atraves de objetos listeners.
+# Cenario pos-configuracao da conexao, quando deve ser possivel escutar mensagens
+# atraves de objetos listeners (especializacoes de Listener).
 #
+describe JSparrow::Connection::Listener,
+         ', quando especializado para escutar uma queue,' do
+  
+  before(:all) do
+    @jms_listener = create_jms_listener
+  end
+  
+  it 'deveria ter uma connection_factory especifica para queues' do
+    @jms_listener.jndi_name_of_connection_factory.should_not be nil
+  end
+end
+
+#---
+
 describe JSparrow::Connection::Listener,
          ', quando um Listener se registra para escutar uma Queue especifica,' do
   
